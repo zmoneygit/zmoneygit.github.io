3e91e15c90bce170c6b23b56ed492cda7ef8ef18
zmoneygit <150419805+zmoneygit@users.noreply.github.com>
2023-12-30 15:02:17 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/box-model.png") {
+        myImage.setAttribute("src", "images/R-C.jpg");
+    } else {
+        myImage.setAttribute("src", "images/box-model.png");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+    
